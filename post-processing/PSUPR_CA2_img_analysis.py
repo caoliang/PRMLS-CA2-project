7a86031c01b9f7b1dfd6754a3e25af9ecd642fc6
caoliang <sg_caoliang@yahoo.com.sg>
2019-09-27 00:33:59 +0800

    Add img_analysis app to check image data at post-processing stage; Update assignment report;

--- post-processing/PSUPR_CA2_img_analysis.py
+++ post-processing/PSUPR_CA2_img_analysis.py
@@ -13,7 +13,6 @@ import matplotlib.pylab as plb
 import pandas as pd
 import datetime as dt
 import h5py
-import cv2
 import os
 
 from glob import glob 
@@ -27,54 +26,13 @@ from tensorflow.keras.layers import BatchNormalization
 from tensorflow.keras.layers import Activation
 from tensorflow.keras.layers import AveragePooling2D
 from tensorflow.keras.layers import add
+from tensorflow.keras.layers import concatenate
+from tensorflow.keras.layers import Dropout
 from tensorflow.keras.regularizers import l2
 from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.utils import plot_model
 from tensorflow.keras import optimizers
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
-def read_img_h5py(h5_file='data.h5'):
-    data_set = None
-    lbl_set = None
-    test_index = 0
-    
-    with h5py.File(h5_file, 'r') as hf:
-              
-        start_index = int(hf['start_index'].value)
-        end_index = int(hf['end_index'].value)
-        print('start_index:', start_index, 'end_index:', end_index)
-            
-        for i in range(start_index, end_index):
-            test_index += 1
-            
-            img_raw = hf['X'+str(i)]
-            img_data = np.array(img_raw[:,:,:])
-            img_data = cv2.cvtColor(img_data, cv2.COLOR_BGR2RGB)
-            
-            img_data = img_data.reshape(-1, 128, 128, 3)
-            
-            img_lbl = np.array(hf['y'+str(i)].value)
-            
-            img_id = np.array(hf['id'+str(i)].value)
-            
-            if data_set is None:
-                data_set = img_data
-                lbl_set = img_lbl
-                id_set = img_id
-            else:
-                data_set = np.concatenate((data_set, img_data))
-                lbl_set = np.concatenate((lbl_set, img_lbl))
-                id_set = np.concatenate((id_set, img_id))
-            
-            if test_index % 1000 == 0:
-                print('img data shape:', img_data.shape)
-                print('data_set shape', data_set.shape)
-                print('img_lbl:', img_lbl)
-                print('lbl_set shape:', lbl_set.shape)
-                print('img_id:', img_id)
-                print('id_set shape:', id_set.shape)
-                
-        
-    return (data_set, lbl_set, id_set) 
 
 def implt(img):
     plt.figure()
@@ -90,26 +48,157 @@ plt.rcParams['ytick.labelleft'] = False
 plt.rcParams['font.family']     = 'Arial'
 
 def read_data_set(h5_file='out.h5'):
-    data_map = { 'cat':0, 'bird':1, 'dog':2 }
-    
+        
     with h5py.File(h5_file, 'r') as hf:
         X_train = hf['X_train'].value
-        
-        y_train = pd.Series(hf['y_train']).map(data_map)
-        print('Read y_train: ', y_train.shape)        
+        y_train = hf['y_train'].value
         
         X_test = hf['X_test'].value
+        y_test = hf['y_test'].value
         
-        y_test = pd.Series(hf['y_test']).map(data_map)
+        X_val = np.concatenate((X_train, X_test))
+        print('Read X_val: ', X_val.shape)        
         
-    return (X_train, y_train, X_test, y_test)
+        y_val = np.concatenate((y_train, y_test))
+        print('Read y_val: ', y_val.shape)        
+                
+    return (X_val, y_val)
+
+def createResNetV1_7631(inputShape=(128,128,3),
+                        numClasses=3):
+  inputs = Input(shape=inputShape)
+  v = resLyr(inputs,
+            lyrName='Input')
+  
+  v = Dropout(0.2)(v)
+  
+  v = resBlkV1(inputs=v,
+              numFilters=16,
+              numBlocks=7,
+              downsampleOnFirst=False,
+              names='Stg1')
+  
+  v = Dropout(0.2)(v)
+  
+  v = resBlkV1(inputs=v,
+              numFilters=32,
+              numBlocks=7,
+              downsampleOnFirst=True,
+              names='Stg2')
+  
+  v = Dropout(0.2)(v)
+  
+  v = resBlkV1(inputs=v,
+              numFilters=64,
+              numBlocks=7,
+              downsampleOnFirst=True,
+              names='Stg3')
+  
+  v = Dropout(0.2)(v)
+  
+  v = resBlkV1(inputs=v,
+              numFilters=128,
+              numBlocks=7,
+              downsampleOnFirst=True,
+              names='Stg4')
+  
+  v = Dropout(0.2)(v)
+  
+  v = AveragePooling2D(pool_size=8,
+                      name='AvgPool')(v)
+  
+  v = Dropout(0.2)(v)
+  
+  v = Flatten()(v)
+  outputs = Dense(numClasses,
+                 activation='softmax',
+                 kernel_initializer='he_normal')(v)
+  model = Model(inputs=inputs,outputs=outputs)
+  model.compile(loss='categorical_crossentropy',
+               optimizer=optmz,
+               metrics=['accuracy'])
+    
+  return model
+
+# load models from file
+def load_all_models(n_models, model_name='PRMLS_CA2_Ensemble_NN',
+                    model_files=[], models_creation_func=[]):
+    all_models = list()
+    for i in range(n_models):
+        # define filename for this ensemble
+        if len(model_files) == n_models:
+            filename = model_files[i]
+        else:
+            filename = model_name + '_' + str(i + 1) + '.hdf5'
+        
+        # create model from model creation function list
+        if len(models_creation_func) == n_models:
+            model = models_creation_func[i]()
+        else:
+            model = createResNetV1_7631()
+        
+        # load model weight from file
+        model.load_weights(filename)
+        
+        # add to list of members
+        all_models.append(model)
+        print('>loaded %s' % filename)
+    return all_models
+ 
+# define stacked model from multiple member input models
+def define_stacked_model(members, numClasses=3,
+                         out_model_img=None,
+                         model_optimizer='adam'):
+    # update all layers in all models to not be trainable
+    for i in range(len(members)):
+        model = members[i]
+        for layer in model.layers:
+            # rename to avoid 'unique layer name' issue
+            #print(layer.__dict__)
+            layer._name = 'ensemble_' + str(i+1) + '_' + layer.name            
+            # make not trainable
+            layer.trainable = False
+
+    # define multi-headed input
+    ensemble_visible = [model.input for model in members]
+    # concatenate merge output from each model
+    ensemble_outputs = [model.output for model in members]
+    merge = concatenate(ensemble_outputs)
+    hidden = Dense(len(members) * numClasses, activation='relu')(merge)
+    output = Dense(numClasses, activation='softmax')(hidden)
+    model = Model(inputs=ensemble_visible, outputs=output)
+    
+    # plot graph of ensemble
+    if out_model_img is not None:
+        plot_model(model, to_file=out_model_img, show_shapes=True, 
+                   show_layer_names=True)
+
+    # compile
+    model.compile(loss='categorical_crossentropy', optimizer=model_optimizer, 
+                  metrics=['accuracy'])
+    return model
+
+# ----------------------------------
+# All files pathes are defined below
+# ----------------------------------
+# Number of models
+n_members = 2
+# Ensemble model name
+modelname   = 'PRMLS_CA2_Ensemble_NN'
+# Images data hdf5 file
+img_data_h5_file = '..\\ca2data.h5'
+# model hdf5 files
+model_files_list = [ '..\\models\\' + modelname + '_1.hdf5', 
+                     '..\\models\\' + modelname + '_2.hdf5' ]
+# model creation function list
+model_create_list = [ createResNetV1_7631, createResNetV1_7631 ]
+# Ensemble model hdf5 file
+filepath        = '..\\models\\' + modelname + ".hdf5"
 
 
-h5_file = 'img_128.h5'
-X_test_data, y_test_data, img_id_data = read_img_h5py(h5_file=h5_file)
+X_test_data, y_test_data = read_data_set(h5_file=img_data_h5_file)
 print('Read X_test_data: ', X_test_data.shape)        
 print('Read y_test_data: ', y_test_data.shape)        
-print('Read img_id_data: ', img_id_data.shape)        
     
 (tsDat, tsLbl)  = X_test_data, y_test_data
 print('Read tsDat: ', tsDat.shape)
@@ -140,7 +229,6 @@ optmz       = optimizers.Adam(lr=0.0001)
 #optmz       = optimizers.RMSprop(lr=0.0002, rho=0.9, 
 #                                 epsilon=1e-08, decay=0.01/4000)
 
-modelname   = 'PRMLS_CA2'
                             # define the deep learning model
 
 def resLyr(inputs,
@@ -260,27 +348,26 @@ def save_img_id_list(out_file='list.txt', img_id_list=[]):
 
 
                                 # Setup the models
-modelGo     = createResNetV1()  # This is used for final testing
-modelGo.summary()
+optmz       = optimizers.Adam(lr=0.0001)
 
-                            # Create checkpoint for the training
-                            # This checkpoint performs model saving when
-                            # an epoch gives highest testing accuracy
 
-model_trained = 'PRMLS_CA2_74.54'
-filepath        = model_trained + ".hdf5"
-                            # Now the training is complete, we get
-                            # another object to load the weights
-                            # compile it, so that we can do 
-                            # final evaluation on it
-modelGo.load_weights(filepath)
+                            # define the deep learning model
+# load all models
+members = load_all_models(n_members, model_files=model_files_list,
+                          models_creation_func=model_create_list)
+print('Loaded %d models' % len(members))
 
-modelGo.compile(loss='categorical_crossentropy', 
-                optimizer=optmz, 
-                metrics=['accuracy'])
+X_tsDat = [tsDat for model in members]
+print("X_tsDat: ", np.array(X_tsDat).shape)
+
+stacked_model_go = define_stacked_model(members, numClasses=num_classes,
+                                        model_optimizer=optmz)                        
+stacked_model_go.load_weights(filepath)
+print('Loaded stacked model')
+
 
                             # Make classification on the test dataset
-predicts    = modelGo.predict(tsDat)
+predicts    = stacked_model_go.predict(X_tsDat)
 print("predicts: ", predicts.shape)
 print(predicts[:3, :3])
 print("predicts max: ", predicts.max())
@@ -304,13 +391,11 @@ print("Best accuracy (on testing dataset): %.2f%%" % (testScores*100))
 print(metrics.classification_report(testout,predout,target_names=labelname,digits=4))
 print(confusion)
 
-testout_df_data = img_id_data.reshape([len(img_id_data), 1])
-testout_df_data = np.concatenate((testout_df_data, tsLbl), axis=1)
-testout_df_data = np.concatenate((testout_df_data, predicts), axis=1)
+testout_df_data = np.concatenate((tsLbl, predicts), axis=1)
 print('testout_df_data: ', testout_df_data.shape)
 
 testout_df = pd.DataFrame(data=testout_df_data, 
-                          columns=['img_id', 'img_cat', 'img_bird', 'img_dog', 
+                          columns=['img_cat', 'img_bird', 'img_dog', 
                                    'cat', 'bird', 'dog'])
 print('testout_df: ', testout_df.shape)
 #testout_df.set_index('img_id', inplace=True)
@@ -329,41 +414,53 @@ testout_df['class_name'] = ''
 testout_df.loc[testout_df.img_cat == 1, 'class_name'] = 'cat'
 testout_df.loc[testout_df.img_bird == 1, 'class_name'] = 'bird'
 testout_df.loc[testout_df.img_dog == 1, 'class_name'] = 'dog'
+print(testout_df.head(n=3))
 
 testout_df = testout_df.sort_values(by=['prob'], ascending=True)
 print('testout_df: ', testout_df.shape)
 
-unfit_threshold = 0.1
+fit_step = 0.1
+from_fit = 0
+to_fit = 0
+fit_count_list = []
+fit_count_lbl = []
+fit_count_class = []
 
-unfit_img_df = testout_df[testout_df.prob < unfit_threshold]
-print("unfit_img_df: ", unfit_img_df.shape)
+fit_list = []
+class_names = ['cat', 'bird', 'dog']
+row_index = 0
+    
+for i in np.arange(0, 1, fit_step):
+    from_fit = i
+    to_fit = from_fit + fit_step
+    
+    fit_range = '{0:.1f}-{1:.1f}'.format(from_fit, to_fit)
+    fit_img_df = testout_df[testout_df['prob'] > from_fit]
+    fit_img_df = fit_img_df[testout_df['prob'] <= to_fit]
+        
+    for class_n in class_names:
+        fit_class_df = fit_img_df[fit_img_df['class_name'] == class_n]
+        fit_count = fit_class_df.shape[0]
+        fit_row = {'quantity': fit_count, 'range': fit_range,
+                    'class_name': class_n}
+        print('add fit_row: ', row_index)
+        fit_list.append(fit_row)
+        print(len(fit_list))
+        row_index += 1
 
-fit_img_df = testout_df[testout_df.prob >= unfit_threshold]
-print("fit_img_df: ", fit_img_df.shape)
+fit_df = pd.DataFrame(fit_list)
+print(fit_df.shape)
+color_list = ['#CC00CC', '#0000BB', 'green']
+              
+for i, class_n in enumerate(class_names):
+    fit_class_df = fit_df[fit_df['class_name'] == class_n]
+    chart_title = '{} Image Data'.format(class_n.capitalize())
+     
+    ax = fit_class_df.plot(kind='bar', x='range', y='quantity', 
+                     rot=0, colors=color_list[i], title=chart_title,
+                     figsize=(10,5))
+    for p in ax.patches:
+        ax.annotate(str(p.get_height()), 
+                    (p.get_x() * 1.005, p.get_height() * 1.005))
+    plt.show()
 
-fit_df = pd.DataFrame(data=[['unfit img', unfit_img_df.shape[0]], 
-                            ['fit img', fit_img_df.shape[0]]],
-                      columns=['Image Data', 'Quantity'])
-#print(fit_df['label'])
-
-ax = fit_df.plot(kind='bar', x='Image Data', y='Quantity', rot=0,
-                 colors=['blue', 'blue'])
-for p in ax.patches:
-    ax.annotate(str(p.get_height()), 
-                (p.get_x() * 1.005, p.get_height() * 1.005))
-plt.show()
-
-ax = fit_img_df['class_name'].value_counts().transpose().plot(kind='bar', 
-               rot=0, color=['#CC00CC', '#0000BB', 'green'])
-for p in ax.patches:
-    ax.annotate(str(p.get_height()), 
-                (p.get_x() * 1.005, p.get_height() * 1.005))
-plt.show()
-
-# Save unfit image id list to file
-unfit_img_id_list = list(unfit_img_df['img_id'].values)
-print('unfit_img_id_list: ', len(unfit_img_id_list))
-unfit_img_id_list_file = '..\\unfit_list.txt'
-save_img_id_list(out_file=unfit_img_id_list_file, 
-                 img_id_list=unfit_img_id_list)
-print('Save unfit image id list to file:', unfit_img_id_list_file)
